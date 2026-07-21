60366342f51a2b5a548abd0b25ab3df756bace78
Sai Krishna <saik33319@gmail.com>
2026-07-21 23:25:51 +0530

    Fix user_password variable assignment and call

--- file.py
+++ file.py
@@ -1,7 +1,8 @@
-user_password = "12345"
+user_password = 12345
 user_name = "Sai99953"
 def file(password,name):
     if user_password==password and user_name==name:
         print("Login succesful")
     else:
         print("invalid details")
+file(12345,"Sai99953")
