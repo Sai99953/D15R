4ed2cbab1fa04f7a54a9a517018cafbc23534745
Sai99953 <saik33319@gmail.com>
2026-07-21 22:11:27 +0530

    Added python file

---
+++ file.py
@@ -0,0 +1,7 @@
+user_password = "12345"
+user_name = "Sai99953"
+def file(password,name):
+    if user_password==password and user_name==name:
+        print("Login succesful")
+    else:
+        print("invalid details")
