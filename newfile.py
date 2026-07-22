331bfe0ae16e068d7ed86ff0a19a0ecef9e51a96
Sai99953 <saik33319@gmail.com>
2026-07-22 23:19:47 +0530

    Added another file

---
+++ newfile.py
@@ -0,0 +1,10 @@
+num=int(input("Enter Any number: "))
+
+if num>95:
+    print("Excellent marks")
+elif num>=80:
+    print("Good marks")
+elif num>=65:
+    print("Better marks")
+else:
+    print("Fail")
